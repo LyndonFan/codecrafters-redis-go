7f67c03e1fa958613ea02f7d44e83d9bc18dcb4c
Lyndon Fan <lyndon0808@gmail.com>
2024-04-27 12:40:03 +0100

    CLI tests (cli-test-1714218003272)

--- app/server.go
+++ app/server.go
@@ -6,13 +6,47 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 var port int
 
+type ReplicationRole struct {
+	Host string
+	Port int
+}
+
+func (m *ReplicationRole) IsMaster() bool {
+	return m.Host == "" && m.Port == 0
+}
+
+func (m *ReplicationRole) String() string {
+	if m.IsMaster() {
+		return ""
+	}
+	return fmt.Sprintf("%s:%d", m.Host, m.Port)
+}
+
+func (m *ReplicationRole) Set(value string) error {
+	parts := strings.Split(value, " ")
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid replication master: %s", value)
+	}
+	port, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return err
+	}
+	m.Host = parts[0]
+	m.Port = port
+	return nil
+}
+
+var role ReplicationRole
+
 func init() {
 	flag.IntVar(&port, "port", 6379, "port to listen to")
+	flag.Var(&role, "replicaof", "if specified, the host and port of its master")
 	flag.Parse()
 }
 
@@ -59,8 +93,7 @@ func handleConnection(conn net.Conn) {
 		}
 		response, err := runTokens(tokens)
 		if err != nil {
-			fmt.Println("Error running tokens: ", err.Error())
-			break
+			response = &Token{Type: errorType, SimpleValue: fmt.Sprintf("error: %s", err.Error())}
 		}
 		fmt.Println("Response: ", strings.Replace(response.Value(), TERMINATOR, "\\r\\n", -1))
 

6ec4915ac0dd946a1a443f2b21510b55054aa005
Lyndon Fan <lyndon0808@gmail.com>
2024-04-29 19:40:23 +0100

    feat: finish handshake 3/3

--- app/replication/handshake.go
+++ app/replication/handshake.go
@@ -3,33 +3,27 @@ package replication
 import (
 	"fmt"
 	"net"
+	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/codecrafters-io/redis-starter-go/app/token"
 )
 
-func sendMessage(conn net.Conn, tkn *token.Token, expectedResponse string) error {
+func sendMessage(conn net.Conn, tkn *token.Token) (string, error) {
 	messageString := tkn.EncodedString()
-	fmt.Println("Will send", strings.Replace(messageString, token.TERMINATOR, "\\r\\n", -1))
+	fmt.Println("Will send", replaceTerminator(messageString))
 	_, err := conn.Write([]byte(tkn.EncodedString()))
 	if err != nil {
-		return err
+		return "", err
 	}
 	buf := make([]byte, 128)
 	n, err := conn.Read(buf)
 	if err != nil {
-		return err
+		return "", err
 	}
 	response := string(buf[:n])
-	if response != expectedResponse {
-		return fmt.Errorf(
-			"expected \"%s\", received \"%s\"",
-			strings.Replace(expectedResponse, token.TERMINATOR, "\\r\\n", -1),
-			strings.Replace(response, token.TERMINATOR, "\\r\\n", -1),
-		)
-	}
-	return nil
+	return response, nil
 }
 
 func (r Replicator) HandshakeWithMaster() error {
@@ -53,7 +47,13 @@ func (r Replicator) HandshakeWithMaster() error {
 			},
 		},
 	}
-	err = sendMessage(conn, message, "+PONG"+token.TERMINATOR)
+
+	var response string
+	response, err = sendMessage(conn, message)
+	if err != nil {
+		return err
+	}
+	err = checkString(response, "+PONG"+token.TERMINATOR)
 	if err != nil {
 		return err
 	}
@@ -63,7 +63,11 @@ func (r Replicator) HandshakeWithMaster() error {
 		{Type: token.BulkStringType, SimpleValue: "listening-port"},
 		{Type: token.BulkStringType, SimpleValue: strconv.Itoa(r.Port)},
 	}
-	err = sendMessage(conn, message, token.OKToken.EncodedString())
+	response, err = sendMessage(conn, message)
+	if err != nil {
+		return err
+	}
+	err = checkString(response, token.OKToken.EncodedString())
 	if err != nil {
 		return err
 	}
@@ -73,9 +77,42 @@ func (r Replicator) HandshakeWithMaster() error {
 		{Type: token.BulkStringType, SimpleValue: "capa"},
 		{Type: token.BulkStringType, SimpleValue: "psync2"},
 	}
-	err = sendMessage(conn, message, token.OKToken.EncodedString())
+	response, err = sendMessage(conn, message)
 	if err != nil {
 		return err
 	}
+	err = checkString(response, token.OKToken.EncodedString())
+	if err != nil {
+		return err
+	}
+
+	message.NestedValue = []*token.Token{
+		{Type: token.BulkStringType, SimpleValue: "PSYNC"},
+		{Type: token.BulkStringType, SimpleValue: "?"},
+		{Type: token.BulkStringType, SimpleValue: "-1"},
+	}
+	response, err = sendMessage(conn, message)
+	if err != nil {
+		return err
+	}
+	expectedStringPattern := "\\+FULLRESYNC [a-z0-9]{40} 0\r\n" // need double escape
+	if matched, err := regexp.MatchString(expectedStringPattern, response); !matched || err != nil {
+		return fmt.Errorf("expected response to match \"%s\", got %s", replaceTerminator(expectedStringPattern), replaceTerminator(response))
+	}
 	return nil
+}
+
+func replaceTerminator(x string) string {
+	return strings.Replace(x, token.TERMINATOR, "\\r\\n", -1)
+}
+
+func checkString(actual, expected string) error {
+	if actual == expected {
+		return nil
+	}
+	return fmt.Errorf(
+		"expected \"%s\", received \"%s\"",
+		replaceTerminator(expected),
+		replaceTerminator(actual),
+	)
 }

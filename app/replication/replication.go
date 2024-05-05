343feb65c20fb8dccab0190371c7d4df7ae8f184
Lyndon Fan <lyndon0808@gmail.com>
2024-05-05 18:36:59 +0100

    wip: try get master conn working

--- app/replication/replication.go
+++ app/replication/replication.go
@@ -9,10 +9,6 @@ import (
 
 const EMPTY_RDB_FILE_HEX string = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
 
-// func (r *Replicator) AddFollower(port int) {
-// 	r.FollowerPorts = append(r.FollowerPorts, port)
-// }
-
 func (r *Replicator) ShouldAddConnection(port int) bool {
 	_, exists := r.followerConnections[port]
 	return !exists

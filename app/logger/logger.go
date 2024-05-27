4e8c49d1d345e9bb6e935d4977ffe5fe629abd22
Lyndon Fan <lyndon0808@gmail.com>
2024-05-27 19:18:18 +0100

    CLI tests (cli-test-1716833898366)

--- app/logger/logger.go
+++ app/logger/logger.go
@@ -6,8 +6,24 @@ import (
 	"os"
 )
 
-func NewLogger(port int) *slog.Logger {
+const (
+	LOG_LEVEL_DEBUG string = "debug"
+	LOG_LEVEL_INFO  string = "verbose"
+	LOG_LEVEL_WARN  string = "notice"
+	LOG_LEVEL_ERROR string = "warning"
+)
+
+var levelMapper = map[string]slog.Level{
+	LOG_LEVEL_DEBUG: slog.LevelDebug,
+	LOG_LEVEL_INFO:  slog.LevelInfo,
+	LOG_LEVEL_WARN:  slog.LevelWarn,
+	LOG_LEVEL_ERROR: slog.LevelError,
+}
+
+func NewLogger(port int, logLevel string) *slog.Logger {
+	minLevel := levelMapper[logLevel]
 	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: minLevel,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			if a.Key != slog.MessageKey {
 				return a
